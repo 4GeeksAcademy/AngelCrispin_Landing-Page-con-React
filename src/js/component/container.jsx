0bd83ce925bd85d6299559f46500b37430f4da48
Angel Crispin <155650252+AngelCrispin@users.noreply.github.com>
2024-08-09 23:06:57 +0000

    First Coommit

---
+++ src/js/component/container.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+
+const Container = () =>{
+    return(
+        <>
+            <Jumbotron></Jumbotron>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </>
+    );
+};
+
+export default Container;
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand mx-auto" href="#">Copyright Your Website 2019</a>
+        </nav>
+
+    );
+}
+
+export default Footer;
